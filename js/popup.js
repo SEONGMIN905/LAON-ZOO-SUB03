33aa13c40ce1d5750792ba8b94165de1b1c76754
SEONGMIN905 <111342455+SEONGMIN905@users.noreply.github.com>
2022-08-23 16:21:55 +0900

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,108 @@
+$(function(){
+
+    /*1, 호랑이*/
+
+    $('.plus1 a').click(function(){
+        $('.popup').fadeIn();//.show();
+    });
+    $('.popup a').click(function(){
+        $('.popup').fadeOut();//.hide();
+    });
+
+
+
+    /*2, 사자*/
+
+    $('.plus2 a').click(function(){
+        $('.popup2').fadeIn();//.show();
+    });
+
+    $('.popup2 a').click(function(){
+        $('.popup2').fadeOut();//.hide();
+    });
+
+
+
+    /*3, 코끼리*/
+
+    $('.plus3 a').click(function(){
+        $('.popup3').fadeIn();//.show();
+    });
+
+    $('.popup3 a').click(function(){
+        $('.popup3').fadeOut();//.hide();
+    });
+
+
+    /*4, 기린*/
+
+    $('.plus4 a').click(function(){
+        $('.popup4').fadeIn();//.show();
+    });
+
+    $('.popup4 a').click(function(){
+        $('.popup4').fadeOut();//.hide();
+    });
+
+
+    /*5, 판다*/
+
+    $('.plus5 a').click(function(){
+        $('.popup5').fadeIn();//.show();
+    });
+
+    $('.popup5 a').click(function(){
+        $('.popup5').fadeOut();//.hide();
+    });
+
+
+
+    /*6, 북극곰*/
+
+    $('.plus6 a').click(function(){
+        $('.popup6').fadeIn();//.show();
+    });
+
+    $('.popup6 a').click(function(){
+        $('.popup6').fadeOut();//.hide();
+    });
+
+
+
+    /*7, 북극여우*/
+
+    $('.plus7 a').click(function(){
+        $('.popup7').fadeIn();//.show();
+    });
+
+    $('.popup7 a').click(function(){
+        $('.popup7').fadeOut();//.hide();
+    });
+
+
+
+    /*8, 라쿤*/
+
+    $('.plus8 a').click(function(){
+        $('.popup8').fadeIn();//.show();
+    });
+
+    $('.popup8 a').click(function(){
+        $('.popup8').fadeOut();//.hide();
+    });
+
+
+
+    /*9, 표범*/
+
+    $('.plus9 a').click(function(){
+        $('.popup9').fadeIn();//.show();
+    });
+
+    $('.popup9 a').click(function(){
+        $('.popup9').fadeOut();//.hide();
+    });
+
+  
+
+});
